f69ecb04548c313c3f712569394648be28250978
zhuyunhao <zhuyunhao@inspur.com>
2020-05-15 11:41:38 +0800

    update main.go

--- 2020.5.11/listen4/main.go
+++ 2020.5.11/listen4/main.go
@@ -10,6 +10,21 @@ func main() {
 	fmt.Println(result)
 }
 
+/*
+	题目要求
+
+给你一个数组 arr ，请你将每个元素用它右边最大的元素替换，如果是最后一个元素，用 -1 替换。
+
+完成所有替换操作后，请你返回这个数组。
+
+
+
+示例：
+
+输入：arr = [17,18,5,4,6,1]
+输出：[18,6,6,6,1,-1]
+*/
+
 func replaceElements(arr []int) []int {
 	var length = len(arr)
 	var result = make([]int, length)
