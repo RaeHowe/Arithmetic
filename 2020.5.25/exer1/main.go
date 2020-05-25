bf6fa17eeff574da909b09ec986a9d58e8910864
zhuyunhao <zhuyunhao@inspur.com>
2020-05-25 11:34:09 +0800

    update by RaeHowe at 2020.5.25

--- 2020.5.25/exer1/main.go
+++ 2020.5.25/exer1/main.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
 func main()  {
-	
+	var array = []int{3, 1, 2, 4}
+	result := sortArrayByParity(array)
+	fmt.Println(result)
+}
+
+func sortArrayByParity(A []int) []int {
+	var a = 0
+
+	for ; a < len(A); a++{
+		var b = len(A) - 1
+		if a == b{
+			break
+		}
+		for A[a] % 2 != 0{
+			//奇数，和最后一个元素进行交换
+			A[a], A[b] = A[b], A[a]
+			if a < b{
+				b--
+			}else {
+				break
+			}
+		}
+	}
+
+	return A
 }
